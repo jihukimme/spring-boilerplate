1295d3e971406c0fb3678b45baac2384c2ecea34
jihukimme <jihu0210@naver.com>
2026-01-02 01:43:11 +0900

    chore: 테스트를 위한 코드 삭제

--- application/src/main/java/com/example/application/Application.java
+++ application/src/main/java/com/example/application/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-        System.out.println("TEST DB PASSWORD: " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(Application.class, args);
 	}
 
